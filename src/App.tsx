2bed5c5550aefc11f7b1b543af150dbd6356bb00
Hell Bringer 419 <35007817+HellBringer419@users.noreply.github.com>
2022-01-04 10:17:54 +0530

    added Container structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import * as React from "react"
 import {
   ChakraProvider,
   Box,
@@ -8,31 +7,51 @@ import {
   Code,
   Grid,
   theme,
-} from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import { Logo } from "./Logo"
+  HStack,
+  Container,
+  Center,
+  Stack,
+} from "@chakra-ui/react";
+import { ColorModeSwitcher } from "./ColorModeSwitcher";
 
 export const App = () => (
   <ChakraProvider theme={theme}>
     <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
-        <VStack spacing={8}>
-          <Logo h="40vmin" pointerEvents="none" />
-          <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-          </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Chakra
-          </Link>
+      <Stack minH="100vh" spacing={8} direction={["column", "row"]}>
+        <Center w="66%">
+          <Text> Summary </Text>
+        </Center>
+        <VStack w="34%">
+          <ColorModeSwitcher alignSelf={"flex-end"} />
+          <Center h="80%" w="100%">
+            <Text> Trigger </Text>
+          </Center>
         </VStack>
-      </Grid>
+      </Stack>
     </Box>
   </ChakraProvider>
-)
+);
+
+{
+  /* <Grid minH="100vh" p={3}>
+        <ColorModeSwitcher justifySelf="flex-end" />
+        </Grid> */
+}
+
+{
+  /* <VStack spacing={8}>
+  <Logo h="40vmin" pointerEvents="none" />
+  <Text>
+    Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
+  </Text>
+  <Link
+    color="teal.500"
+    href="https://chakra-ui.com"
+    fontSize="2xl"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    Learn Chakra
+  </Link>
+  </VStack> */
+}
