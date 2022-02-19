453055b54aac8c27a8e04f4b2f05ab6c18e51998
Hell Bringer 419 <35007817+HellBringer419@users.noreply.github.com>
2022-02-19 18:30:06 +0530

    Trigger Reactions hover is now snappier & smoother

--- src/components/Trigger/TriggerEmoji.tsx
+++ src/components/Trigger/TriggerEmoji.tsx
@@ -3,6 +3,7 @@ import {
   PopoverContent,
   PopoverTrigger,
   Text,
+  Tooltip,
   useColorModeValue,
 } from "@chakra-ui/react";
 
@@ -15,32 +16,29 @@ const TriggerEmoji: React.FC<{
   const blackColor = useColorModeValue("#161616", "#E8E8E8");
   const bgWhite = useColorModeValue("white", "gray.800");
   return (
-    <Popover placement="top" trigger="hover">
-      <PopoverTrigger>
-        <Text
-          fontWeight="700"
-          fontSize="14px"
-          lineHeight="18px"
-          _hover={{ fontSize: "32px" }}
-          onClick={() => handleEmojiSelect(id)}
-        >
-          {emoji}
-        </Text>
-      </PopoverTrigger>
-      <PopoverContent
-        h="24px"
-        w="fit-content"
-        borderRadius="100px"
-        bg={blackColor}
-        color={bgWhite}
-        paddingY="2px"
-        paddingX="8px"
-        fontSize="12px"
-        borderWidth="0"
+    <Tooltip
+      h="24px"
+      w="fit-content"
+      borderRadius="100px"
+      bg={blackColor}
+      color={bgWhite}
+      paddingY="2px"
+      paddingX="8px"
+      fontSize="12px"
+      borderWidth="0"
+      label={name}
+      placement="top"
+    >
+      <Text
+        fontWeight="700"
+        fontSize="14px"
+        lineHeight="18px"
+        _hover={{ fontSize: "32px" }}
+        onClick={() => handleEmojiSelect(id)}
       >
-        <Text>{name}</Text>
-      </PopoverContent>
-    </Popover>
+        {emoji}
+      </Text>
+    </Tooltip>
   );
 };
 
